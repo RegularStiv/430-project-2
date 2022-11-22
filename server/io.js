2572d82d862983a5238df76f87a86050f15e264a
RegularStiv <sar7743@rit.edu>
2022-11-21 22:39:58 -0500

    translation works now

--- server/io.js
+++ server/io.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const { Server } = require('socket.io');
+const translatte = require('translatte');
 
 let io;
-const handleChatMessage = (msg) => {
+const handleChatMessage = async (msg) => {
   console.log(msg);
-  io.emit('chat message', msg);
+  
+    let translatedMessage;
+  await translatte(msg, {to: 'ru'}).then(res => {
+    translatedMessage = res.text;
+    io.emit('chat message', `${translatedMessage}(${msg})`);
+}).catch(err => {
+    console.error(err);
+});
 };
 
 const socketSetup = (app) => {
